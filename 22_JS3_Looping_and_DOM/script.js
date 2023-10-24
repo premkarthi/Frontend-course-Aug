0d5af011e11df9b22078a84d38e1993095075eb9
premkarthi <premkarthi1234@example.com>
2023-10-24 14:56:47 +0530

    Looping and DOM CRUD

---
+++ 22_JS3_Looping_and_DOM/script.js
@@ -0,0 +1,131 @@
+
+
+        // let a = 12;
+
+        // switch (a) {
+        //     case 3:
+        //         alert( 'Too small' );
+        //         break;
+        //     case 4:
+        //         alert( 'Exactly!' );
+        //         break;
+        //     case 5:
+        //         alert( 'Too big' );
+        //         break;
+        //     default:
+        //         alert( "I don't know such values" );
+        // }
+
+
+
+
+        // let age = prompt('How old are you?');
+        // console.log(age);
+
+
+        // let isAlive = confirm('Is Alive ???')
+
+        // console.log(isAlive);
+
+
+        // console.log("Hello")
+            
+        // Looping 
+        // 1. Start point
+        // 2. Ending Point 
+        // 3. increment/decrement  or update
+
+
+        // Print Hello 10 times 
+
+        // start = 1 //start point
+        // end = 10 //end point
+        
+        // while(start <= end){
+        //     console.log(start, "Hello");
+        //     start += 1 // updating 
+        // }
+
+
+        // print number btw 1 to 20 ->  but number by 2
+        // 1 ,3, 5 ,7, 9, 11, 13, 15, 17, 19
+
+        // let start = 1
+        // let end = 20 
+
+        // while (start <= end){
+        //     if(start % 2 == 0){
+        //         console.log(start);
+        //     }
+        //     start += 1
+        // }
+
+        // Print even numbers in for loop 1 to 20
+
+        // for(let start = 1; start <= 20; start++){
+        //     if(start % 2 == 0){
+        //         console.log("Forloop", start);
+        //     }
+        // }
+
+
+        // let start = 1
+        // let end = 20 
+
+        // while (start <= end){
+        //     if(start == 10){
+        //         continue
+        //     }
+        //     else{
+        //         console.log(start);
+        //     }
+        //     start += 1
+        // }
+
+
+
+        // for (let start = 1; start < 20; start++) {
+        //     if(start == 10){
+        //         continue
+        //     }
+        //     if(start == 16){
+        //         break
+        //     }
+        //     else{
+        //         console.log(start)
+        //     }
+        // }
+
+
+
+        // console.log("Ended");
+
+
+            
+        let names = ["Harry","Ross","Bruce","Cook","Carolyn","Morgan","Albert","Walker", "Prem"]   
+
+
+        // console.log(arr[0]);
+        // console.log(arr[1]);
+        // console.log(arr[2]);
+
+        // let index = 0
+
+        // while (index < arr.length){
+        //     console.log(arr[index]);
+        //     index += 1
+        // }
+
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(element);
+        // }
+
+
+        // names.forEach(function(item){
+        //     console.log(item);
+        // })
+
+        // for (const name of names) {
+        //     console.log(name);
+        // }
